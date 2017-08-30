a764a9d63d3883a61081b9ed7108984aa67dfb15
maocaorena <maohan111@vip.sina.com>
2017-08-31 00:48:10 +0800

    列表 分页

---
+++ src/components/pages.vue
@@ -0,0 +1,107 @@
+<template>
+    <div class="pages">
+        <div class="middle">
+            <a href="javascript:;">
+                <img src="../assets/img/pages/Page1.png" alt="">
+            </a>
+            <a href="javascript:;">
+                <img src="../assets/img/pages/Page2.png" alt="">
+            </a>
+            <span class="middle-mid">
+                <i class="white">22</i>
+                <i class="all">共 6 页</i>
+            </span>
+            <a href="javascript:;">
+                <img src="../assets/img/pages/Page3.png" alt="">
+            </a>
+            <a href="javascript:;">
+                <img src="../assets/img/pages/Page4.png" alt="">
+            </a>
+            <span>
+                1111
+            </span>
+        </div>
+        <div class="pages-right">
+            1 - 10 &nbsp 共 54 条
+        </div>
+    </div>
+</template>
+<script type="text/javascript">
+    export default{
+        data(){
+            return {
+
+            }
+        },
+        props:[
+
+        ],
+        methods:{
+
+        },
+        created(){
+
+        }
+    }
+</script>
+<style lang="scss" scoped>
+    .pages{
+        position: relative;
+        width: 100%;
+        height: 30px;
+        text-align: center;
+        background: #f1f1f1;
+        .middle{
+            display: inline-block;
+            vertical-align: middle;
+            width: 235px;
+            height: 30px;
+            line-height: 30px;
+            a{
+                display: inline-block;
+                padding: 0 6px;
+                height: 100%;
+            }
+        }
+        .middle-mid{
+            display: inline-block;
+            width: 86px;
+            height: 100%;
+            i{
+                font-style: normal;
+            }
+            .white{
+                display: inline-block;
+                padding: 0 7px;
+                height: 19px;
+                background: #fff;
+                border: 1px solid #e4e4e4;
+                color: #343434;
+                font-size: 12px;
+                line-height: 18px;
+            }
+            .all{
+                color: #6c6c6c;
+                font-size: 12px;
+            }
+        }
+        .pages-right{
+            position: absolute;
+            top: 0;
+            right: 20px;
+            width: 106px;
+            height: 30px;
+            line-height: 30px;
+            text-align: center;
+            font-size: 12px;
+            color: #666666;
+        }
+    }
+    .pages:after{
+        content: "";
+        display: inline-block;
+        height: 100%;
+        width: 0;
+        vertical-align: middle;
+    }
+</style>
--- src/views/monitoringInstall/monitoringInstallList.vue
+++ src/views/monitoringInstall/monitoringInstallList.vue
@@ -1,15 +1,98 @@
 <template>
     <div id="monitoringRunList">
-        monitoringRun
-        监控接装列表
-        <router-link tag="button" to="/monitoringInstall/item">
-            点击去详情
-        </router-link>
+        <div class="rightTabbar">
+            <div class="rt-item rtItemSelect">
+                监控列表
+            </div>
+            <div class="rt-handle">
+                <div class="handle-item">
+                    <button type="button" name="button">搜索</button>
+                </div>
+                <div class="handle-item">
+                    <input type="text" name="" value="">
+                </div>
+                <div class="handle-item">
+                    <button type="button" name="button">批量删除</button>
+                </div>
+                <div class="handle-item">
+                    <button type="button" name="button">增加账号</button>
+                </div>
+            </div>
+        </div>
+        <div class="content">
+			<div class="list-tit">
+				<table class="list" border="1" cellspacing="0" cellpadding="0">
+					<colgroup>
+						<col width="4">
+						<col width="10">
+						<col width="11">
+						<col width="7">
+						<col width="11">
+						<col width="17">
+                        <col width="9">
+						<col width="9">
+						<col width="9">
+					</colgroup>
+					<thead>
+						<tr>
+							<th>序号</th>
+							<th>监控点名称</th>
+							<th>监控器型号</th>
+							<th>监控器UID</th>
+							<th>监控器出厂编号</th>
+                            <th>安装单位</th>
+                            <th>安装日期</th>
+                            <th>备注</th>
+							<th>操作</th>
+						</tr>
+					</thead>
+				</table>
+			</div>
+			<div class="list-container">
+				<table class="list" border="1" cellspacing="0" cellpadding="0">
+					<colgroup>
+						<col width="4">
+						<col width="10">
+						<col width="11">
+						<col width="7">
+						<col width="11">
+						<col width="17">
+                        <col width="9">
+						<col width="9">
+						<col width="9">
+					</colgroup>
+					<tbody class="list-con">
+						<tr v-for="(item,index) of 40" class="list-con-item">
+							<td>
+                                {{index}}
+							</td>
+							<td>
+                                <a href="javascript:;" class="mode">1#风机监控点</a>
+                            </td>
+							<td>ZT-ROMM50-F</td>
+							<td>313D9313D9313D9313D9</td>
+							<td>20170701039</td>
+							<td>浙江冠南能源科技有限公司</td>
+                            <td>2017-05-05</td>
+							<td>--</td>
+							<td>
+                                <a href="javascript:;" class="mode">删除</a>
+                                <a href="javascript:;" class="mode">编辑</a>
+                            </td>
 
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+        <div class="downpage">
+            <pages-v></pages-v>
+        </div>
     </div>
 </template>
 
 <script>
+import pages from '../../components/pages.vue'
 export default {
     data() {
         return {
@@ -17,23 +100,77 @@ export default {
         }
     },
     components:{
-
+        'pages-v' : pages,
     },
     created(){
-        let tabs = [
-            {
-                isurl : 'list',
-                name : '监控列表'
-            }
-        ];
-        this.$store.dispatch('ChangeRightbar',tabs);
-        
+
     }
 }
 </script>
 
 <style lang="scss" scoped>
     #monitoringRunList{
-        color: blue;
+        .rightTabbar{
+            width: 100%;
+            height: 30px;
+            border-bottom: 4px solid #2899ee;
+            .rt-item{
+                margin-right: 3px;
+                float: left;
+                width: 120px;
+                height: 26px;
+                line-height: 26px;
+                text-align: center;
+                border: 1px solid #cfdde7;
+                border-bottom: 0;
+                cursor: pointer;
+                a{
+                    display: block;
+                    width: 100%;
+                    height: 100%;
+                }
+            }
+            .rtItemSelect{
+                background: #2899ee;
+                border-color: #2899ee;
+                color: #fff;
+            }
+            .rt-handle{
+                float: right;
+                width: 400px;
+                height: 30px;
+            }
+            .handle-item{
+                float: right;
+                margin-left: 10px;
+                button{
+                    height: 24px;
+                    line-height: 24px;
+                    padding: 0 7px;
+                    border: 1px solid #CFDDE7;
+                    border-radius: 3px;
+                    background: url(../../assets/img/bg_sheng.png) repeat-x;
+                    font-size: 12px;
+                    color: #2899EE;
+                    cursor: pointer;
+                }
+                input{
+                    padding: 4px;
+                    width: 150px;
+                    height: 24px;
+                    border: 1px solid #CFDDE7;
+                    border-radius: 3px;
+                }
+            }
+        }
+        .downpage{
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            padding-right: 10px;
+            width: 100%;
+            height: 30px;
+            background: #fff;
+        }
     }
 </style>
